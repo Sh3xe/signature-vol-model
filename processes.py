888583bb4176c828d03179fe66e49d24c6d08515
Sh3xe <sh3xe@laposte.net>
2026-02-15 16:14:02 -0500

    added modules to code, added signature fitting

---
+++ processes.py
@@ -0,0 +1,159 @@
+import numpy as np
+import numpy.random as npr
+from signature_core import *
+
+def generate_brownian(time_grid, n_curves):
+	"""
+	Generate n_curves samples of standard brownians noises
+    Args:
+        time_grid: array or 1D-np.ndarray with time values ranging from t0 to t1, used as a basis for the browninan simulation
+        n_curves: number of independent curves to be generated
+    
+    Returns:
+        np.ndarray of size (n_curves, len(time_grid))
+	"""
+
+	return np.cumsum(np.concatenate(
+		(np.zeros((n_curves, 1)), np.sqrt(time_grid[1:] - time_grid[:-1]) * npr.normal(0, 1, (n_curves, len(time_grid)-1)) ),
+		axis=1
+	), axis=1)
+
+def generate_mrgbm(t0, t1, brownian_noises, x0, kappa, etha, theta, alpha):
+	(n_curves, n_divs) = brownian_noises.shape
+	brownian_diffs = (brownian_noises[:,1:] - brownian_noises[:,:-1])
+	process = np.zeros( (n_curves, n_divs) )
+	dt = (t1-t0)/(n_divs-1)
+	process[:, 0] = x0
+	for i in range(1, process.shape[1]):
+		process[:, i] = process[:, i-1] + kappa * (theta - process[:, i-1]) * dt + (etha + alpha * process[:, i-1])*brownian_diffs[:, i-1]
+
+	return process
+
+def generate_ornstein_uhlenbeck(t0, t1, brownian_noises, x0, kappa, etha, theta):
+	(n_curves, n_divs) = brownian_noises.shape
+	brownian_diffs = (brownian_noises[:,1:] - brownian_noises[:,:-1])
+	process = np.zeros( (n_curves, n_divs) )
+	dt = (t1-t0)/(n_divs-1)
+	process[:, 0] = x0
+	for i in range(1, process.shape[1]):
+		process[:, i] = process[:, i-1] + kappa * (theta - process[:, i-1]) * dt + etha*brownian_diffs[:, i-1]
+
+	return process
+
+def compute_ornstein_uhlenbeck_signature(x0, kappa, etha, theta, exp_order=5):
+	left = Sig([ np.array(x0), np.array([kappa*theta, etha]) ], 2)
+	# compute exp(-kappa 1)
+	right = [np.array(1.0)]
+	for n in range(1,exp_order):
+		right.append( make_canonical_element(tuple(0 for _ in range(n)), 2) * np.power(-kappa, n))
+	return left * Sig(right, 2)
+
+def compute_ornstein_uhlenbeck_signature_time(t, x0, kappa, etha, theta, exp_order=5):
+	assert exp_order >= 2
+	# the scalar part of the signature
+	scalar_part = Sig([theta + np.exp(-kappa*t)*(x0-theta), np.zeros(2)],2)
+	# left hand side of the decomposition: exp(+kappa 1)
+	expon = [np.array(1.0)]
+	for n in range(1, exp_order):
+		expon.append( make_canonical_element(tuple(0 for _ in range(n)), 2) * np.power(kappa, n))
+	# right hand side of the decomposition: etha 2
+	rhs = Sig([np.zeros( () ), np.array([0.0, etha*np.exp(-kappa*t)]) ])
+	# final product
+	return scalar_part + (Sig(expon) * rhs)
+
+def generate_naive_ornstein_sig(time_grid, brownians, brownian_sig, x0, kappa, etha, theta, max_sig_order):
+	ornsteins = generate_ornstein_uhlenbeck(time_grid.min(), time_grid.max(), brownians, x0, kappa, etha, theta)
+	true_uhlenbeck_sig = compute_ornstein_uhlenbeck_signature(x0, kappa, etha, theta, max_sig_order)
+	ornsteins_approx = []
+	for sig_order in range(1, max_sig_order+1):
+		# compute the ornstein process using the signature volatility model
+		true_uhlenbeck_process_sig = bracket_with_process(true_uhlenbeck_sig, brownian_sig, sig_order+1)
+		ornsteins_approx.append(true_uhlenbeck_process_sig)
+
+	return (ornsteins, ornsteins_approx)
+
+def generate_stable_ornstein_sig(time_grid, brownians, brownian_sig, x0, kappa, etha, theta, max_sig_order):
+	ornsteins = generate_ornstein_uhlenbeck(time_grid.min(), time_grid.max(), brownians, x0, kappa, etha, theta)
+	ornstein_approx = []
+	uhlenbeck_sig_time = []
+	for t in time_grid:
+		uhlenbeck_sig_time.append(
+			compute_ornstein_uhlenbeck_signature_time(t, x0, kappa, etha, theta, max_sig_order)
+		)
+
+	for sig_order in range(1, max_sig_order+1):
+		# compute the ornstein process using the signature volatility model
+		uhlenbeck_process_approx = np.zeros(time_grid.shape[0])
+		for i in range(time_grid.shape[0]):
+			brownian_sig_time = [b[..., i] for b in brownian_sig]
+			uhlenbeck_process_approx[i] = bracket(uhlenbeck_sig_time[i][:sig_order+1], brownian_sig_time)
+		ornstein_approx.append(uhlenbeck_process_approx)
+
+	return (ornsteins, ornstein_approx)
+
+def compute_mrgbm_signature(x0, kappa, etha, theta, alpha, exp_order=5):
+	assert exp_order >= 2
+	# left hand side ...
+	lhs = Sig([np.array(x0), np.array([kappa*theta - (alpha*etha)/2, etha])], 2)
+
+	exponent = np.array([-(kappa + (alpha**2)/2), alpha])
+	rhs = Sig([ np.array(1.0), exponent], 2)
+
+	# compute the appoximation of exp( -exponent )
+	curr_num = exponent
+	curr_den = 1
+	for n in range(2, exp_order+1):
+		curr_num = shuffle_product(exponent, curr_num)
+		curr_num_sig = [ np.zeros( tuple(2 for _ in range(i)) ) for i in range(len(curr_num.shape)+1) ]
+		curr_den *= n
+		
+		curr_num_sig[len(curr_num.shape)] = curr_num / curr_den
+		rhs += Sig(curr_num_sig)
+
+	return lhs * rhs	
+
+def generate_naive_mrgbm_sig(time_grid, brownians, brownian_sig, x0, kappa, etha, theta, alpha, max_sig_order):
+	mrgbms = generate_mrgbm(time_grid.min(), time_grid.max(), brownians, x0, kappa, etha, theta, alpha)
+	true_mrgbm_sig = compute_mrgbm_signature(x0, kappa, etha, theta, alpha, max_sig_order)
+
+	mrgbm_approx = []
+	for sig_order in range(1, max_sig_order+1):
+		# compute the mrgbm process using the signature volatility model
+		true_mrgbm_process_sig = bracket_with_process(true_mrgbm_sig.data[:sig_order+1], brownian_sig)
+		mrgbm_approx.append(true_mrgbm_process_sig)
+
+	return (mrgbms, mrgbm_approx)
+
+def compute_mrgbm_signature_times(time_grid, x0, kappa, etha, theta, alpha, lamb, exp_order=5):
+	lhs = compute_mrgbm_signature(x0, kappa, etha, theta, alpha, exp_order)
+	lhs += Sig([np.array(-theta)], 2)
+	# compute exp(lamb 1)
+	rhs = [np.array(1.0)]
+	for n in range(1,exp_order):
+		rhs.append( make_canonical_element(tuple(0 for _ in range(n)), 2) * np.power(lamb, n))
+	
+	prod = lhs.shuffle(Sig(rhs))
+	theta_scalar = Sig([np.array(theta)], 2)
+	values = []
+	for t in time_grid:
+		rest = scalar_prod( prod,  np.exp(-lamb*t) )
+		rest += theta_scalar
+		values.append(rest)
+
+	return values
+
+def generate_stable_mrgbm_sig(time_grid, brownians, brownian_sig, x0, kappa, etha, theta, alpha, max_sig_order):
+	lamb = kappa + (alpha**2)/2
+	mrgbms = generate_mrgbm(time_grid.min(), time_grid.max(), brownians, x0, kappa, etha, theta, alpha)
+	mrgbms_approx = []
+	mrgbm_sig_time = compute_mrgbm_signature_times(time_grid, x0, kappa, etha, theta, alpha, lamb, max_sig_order)
+
+	for sig_order in range(1, max_sig_order+1):
+		# compute the ornstein process using the signature volatility model
+		mrgbm_process_approx = np.zeros(time_grid.shape[0])
+		for i in range(time_grid.shape[0]):
+			brownian_sig_time = [b[..., i] for b in brownian_sig]
+			mrgbm_process_approx[i] = bracket(mrgbm_sig_time[i].data[:sig_order+1], brownian_sig_time)
+		mrgbms_approx.append(mrgbm_process_approx)
+
+	return (mrgbms, mrgbms_approx)
---
+++ signature_core.py
@@ -0,0 +1,203 @@
+import numpy as np
+import numpy.random as npr
+from functools import cache
+
+def is_signature(tensor_list):
+	dim = None
+	for i in range(len(tensor_list)):
+		if len(tensor_list[i].shape) != i:
+			return False
+		if dim == None and i != 0:
+			dim = tensor_list[i].shape[0]
+		if dim != None and tensor_list[i].shape != (dim,)*i:
+			return False
+	return True
+
+class Sig:
+	def __init__(self, tensor_list: list[np.ndarray], dim=None):
+		assert is_signature(tensor_list), "tensor_list is not a signature"
+
+		if len(tensor_list) >= 2:
+			self.dim = tensor_list[1].shape[0]
+			assert dim == None or dim == self.dim, "dimension given in the parameters is inconsistent with the dimension of tensor_list"
+		else:
+			self.dim = dim
+			assert dim is not None, "could not deduce signature dimension"
+		
+		self.data = tensor_list
+
+	def __add__(self, other):
+		assert self.dim == other.dim, "cannot add two signature of different signatures"
+		
+		added_sigs = []
+		for i in range(max(len(self.data), len(other.data))):
+			if i >= len(self.data):
+				added_sigs.append( other.data[i] )
+			elif i >= len(other.data):
+				added_sigs.append( self.data[i] )
+			else:
+				added_sigs.append( self.data[i] + other.data[i] )
+
+		# scalar np.array sums to scalar, we need to convert it back to array
+		added_sigs[0] = np.array(added_sigs[0])
+		return added_sigs
+
+	def __iadd__(self, other):
+		assert self.dim == other.dim, "cannot add two signature of different signatures"
+		
+		# Extend in-place if necessary
+		if len(other.data) > len(self.data):
+			self.data.extend( [ np.zeros( (self.dim,)*order ) for order in range(len(self.data), len(other.data)+1) ] )
+		
+		# Sum ...
+		for i in range(len(other.data)):
+			self.data[i] += other.data[i]
+
+		return self
+
+	def __mul__(self, other):
+		assert self.dim == other.dim, "cannot add two signature of different signatures"
+
+		sig_prod = [ np.zeros(tuple(self.dim for _ in range(n))) for n in range(len(self.data) + len(other.data)) ]
+		for i in range(len(self.data)):
+			for j in range(len(other.data)):
+				sig_prod[i+j] += np.tensordot(self.data[i], other.data[j], axes=0)
+		
+		return Sig(sig_prod)
+
+	def order(self):
+		return len(self.data)
+
+	def __len__(self):
+		return len(self.data)
+
+	def __getitem__(self, index):
+		assert index >= 0, "Index of a signature must be positive"
+		return self.data[index] if index < len(self.data) else np.zeros( (self.dim,)*index )
+
+	def shuffle(self, other, trunc=None):
+		assert self.dim == other.dim, "cannot add two signature of different signatures"
+
+		if trunc is not None:
+			raise NotImplementedError("trunc not yet supported")
+
+		max_order = len(self.data) + len(other.data)
+		
+		shuffle_sig = [ np.zeros( tuple(self.dim for _ in range(n)) ) for n in range(max_order+1) ]
+
+		for i in range(len(self.data)):
+			for j in range(len(other.data)):
+				tensor_order = len(self.data[i].shape) + len(other.data[j].shape)
+				prod_shape = tuple(self.dim for _ in range(tensor_order))
+				
+				# the products will all be a tensor of the same dim and order
+				for self_index in np.ndindex(self.data[i].shape):
+					for other_index in np.ndindex(other.data[j].shape):
+						el = self.data[i][self_index] * other.data[j][other_index] * shuffle_product_basis(tuple(self_index), tuple(other_index), self.dim)
+						shuffle_sig[len(prod_shape)] += el
+
+		return Sig(shuffle_sig)
+
+@cache
+def make_canonical_element(indices, n):
+	x = np.zeros([n for _ in range(len(indices))])
+	x[tuple(indices)] = 1
+	return x
+
+@cache
+def shuffle_product_basis(left: tuple[int], right: tuple[int], n: int) -> list[np.ndarray]:
+	"""
+	Perform the shuffle product between two signature basis
+
+	Args:
+		left, right: "word", i.e. any element of the canonical basis of any tensor of any order, with dimension equal to n.
+		n: dimension of the tensors
+	"""
+	
+	if len(left) == 0:
+		return make_canonical_element(right, n)
+	elif len(right) == 0:
+		return make_canonical_element(left, n)
+	
+	(begin_left, last_left) = left[:-1], left[-1]
+	(begin_right, last_right) = right[:-1], right[-1]
+
+	sub_left = shuffle_product_basis(begin_left, right, n)
+	sub_right = shuffle_product_basis(left, begin_right, n)
+
+	return (
+		np.tensordot(sub_left, make_canonical_element( (last_left,), n), axes=0) +
+		np.tensordot(sub_right, make_canonical_element( (last_right,), n), axes=0)
+	)
+
+def scalar_prod(sig: Sig, scalar):
+		out_sig = []
+		for i in range(len(sig.data)):
+			out_sig.append(sig.data[i] * scalar)
+		return Sig(out_sig)
+
+def shuffle_product(left: np.ndarray, right: np.ndarray) -> list[np.ndarray]:
+	# Special case: scalar
+	if len(left.shape) == 0 and len(right.shape)  == 0:
+		return np.array(left * right)
+	
+	# guess the dimension of the product
+	if len(left.shape) == 0:
+		dim = right.shape[0]
+	else:
+		dim = left.shape[0]
+		
+	# some utility values on the tensors (dimension, trucation of output signature)
+	tensor_order = len(left.shape) + len(right.shape)
+
+	# the products will all be a tensor of the same dim and order
+	product_tensor = np.zeros( tuple(dim for _ in range(tensor_order)) )
+	for self_index in np.ndindex(left.shape):
+		for other_index in np.ndindex(right.shape):
+			el = left[self_index] * right[other_index] * shuffle_product_basis(tuple(self_index), tuple(other_index), dim)
+			product_tensor += el
+
+	return product_tensor
+
+def signature( x: np.ndarray, trunc: int ):
+	"""
+	Compute the truncated signature of x, where x is a sample of some stochastic process
+	trunc is the maximum tensor order of the signature
+	"""
+	assert trunc >= 0, "trunc is the maximum tensor order, it cannot be negative"
+
+	n_divs, dim = x.shape[1], x.shape[0]
+	sig = [np.ones(n_divs), x]
+
+	if trunc <= 2:
+		return sig[:trunc+1]
+
+	# Compute the next signature recursively
+	for n in range(2, trunc+1):
+		# numpy shape corresponding to the nth tensor of the signature
+		shape_n = tuple(dim for _ in range(n))
+		sig_n = np.zeros( shape=shape_n+(n_divs,) )
+		for index in np.ndindex(shape_n):
+			mean_vals = (sig[n-1][index[:-1]][1:] + sig[n-1][index[:-1]][:-1]) / 2
+			integral = mean_vals * (x[index[-1], 1:] - x[index[-1], :-1])
+			sig_n[index] = np.concatenate( ([0], integral.cumsum()) )
+		sig.append(sig_n)
+	return sig
+
+def bracket(sig_left, sig_right):
+	bracket_sum = 0.0
+	for i in range(min(len(sig_left), len(sig_right))):
+		bracket_sum += (sig_left[i] * sig_right[i]).sum()
+	return bracket_sum
+
+def bracket_with_process(cst_sig, process_sig, max_order = None):
+	if max_order == None:
+		max_order = min(len(cst_sig), len(process_sig))
+	else:
+		max_order = min(len(cst_sig), len(process_sig), max_order)
+
+	bracket_sum = np.zeros(process_sig[0].shape)
+	for i in range(max_order):
+		for index in np.ndindex(cst_sig[i].shape):
+			bracket_sum += cst_sig[i][index] * process_sig[i][index]
+	return bracket_sum
